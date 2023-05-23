576ca4552e1d53b36332ca9d911201831b8c9b2c
Yan Li <35964950+yli4924@users.noreply.github.com>
2023-05-23 11:06:03 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,8 @@ import pandas as pd
 import numpy as np
 
 st.title('Posters submission system')
-
+st.button('china')
+st.button('us')
 DATE_COLUMN = 'date/time'
 DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
             'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
